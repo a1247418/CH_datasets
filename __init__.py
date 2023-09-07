3e6089b899a122bb974aa22d8c7de01c46d90f56
llorenz <l.linhardt@campus.tu-berlin.de>
2023-09-07 14:23:37 +0200

    Temporary fix for import resolution

--- __init__.py
+++ __init__.py
@@ -1,2 +1,26 @@
 import sys
-sys.modules["CH_datasets"] = sys.modules["CH_datasets.CH_datasets"]
+
+from .CH_datasets import utils
+try:
+	sys.modules["CH_datasets.utils"] = sys.modules["CH_datasets.CH_datasets.utils"]
+except Exception as e: print(repr(e))
+
+from .CH_datasets import poisoner
+try:
+	sys.modules["CH_datasets.poisoner"] = sys.modules["CH_datasets.CH_datasets.poisoner"]
+except Exception as e: print(repr(e))
+
+from .CH_datasets import datasets
+try:
+	sys.modules["CH_datasets.datasets"] = sys.modules["CH_datasets.CH_datasets.datasets"]
+except Exception as e: pass
+
+from .CH_datasets import scenario
+try:
+	sys.modules["CH_datasets.scenario"] = sys.modules["CH_datasets.CH_datasets.scenario"]
+except Exception as e: pass
+
+from .CH_datasets import scenario_examples
+try:
+	sys.modules["CH_datasets.scenario_examples"] = sys.modules["CH_datasets.CH_datasets.scenario_examples"]
+except Exception as e: pass
